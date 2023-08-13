fbe77b8617c2990775f7d56fc0365a3708d6e3c7
MihaiMocanuGit <81875967+MihaiMocanuGit@users.noreply.github.com>
2023-08-13 15:42:40 +0300

    Update AppConfig.cs

--- Assets/Emotiv/Plugins/AppConfig.cs
+++ Assets/Emotiv/Plugins/AppConfig.cs
@@ -10,11 +10,11 @@ static class AppConfig
     /// Name of directory where contain tmp data and logs file.
     /// </summary>
     public static string TmpAppDataDir       = "DYFtemp";
-    //public static string ClientId            = "GjdUNSFw9QZd6zmSRRXLR7rrfmdKntYOFguR2CZ2";
-    public static string ClientId            = "sIClwpXyw67vK0GLgQfy01XNxzelDMGwbZI46eNx";
+    //public static string ClientId            = "";
+    public static string ClientId            = "";
 
-    //public static string ClientSecret        = "zoTEdYFwMJhzXHTboQPRA0F9IH71YJD0P58rPEOmI9m1KLWQFUxzKXb1LOJWgMMCjCkQotCvjrF4bQKOojsLwN1CLkA5I0zTAcdt4teF2LhkMNr1JBI9Q95Ge0dUJuVL";
-    public static string ClientSecret = "WtlM8h6xSeCeNkxdSK3BQGZIwPqDzH4dKFyQTCtzZTRHU5iqz5Vl5OEWxbIjKkWzfXAQjkl3OMMl3tY72zYRNmESGWADzJezptGuAYqRPpe8vDWutxXrLSXQj1v1pb7T";
+    //public static string ClientSecret        = "";
+    public static string ClientSecret = "";
 
     public static string AppVersion          = "3.0";
     
